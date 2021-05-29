c2a821ac37c131ce3a38f01b259a22cfe72ef77c
Dimas Sudjito <almuhtad@ualberta.ca>
2021-05-28 19:45:53 -0600

    Add semantic ui

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useQuery } from '@apollo/client'
+import gql from 'graphql'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
